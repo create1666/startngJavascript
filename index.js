6948fa5df1cd6ae98b950791d9df7671b49f39ae
OSAKINLE BOLUWAJI JOHN <create1666@gmail.com>
2020-03-25 06:21:00 +0100

    js task1

---
+++ index.js
@@ -0,0 +1,17 @@
+let name = "Osakinle Boluwaji";
+console.log("my name is: " + name);
+
+const courses = ["html","css","javascript","nodejs"];
+
+for(i=0; i<courses.length; i++){
+    console.log(courses[i]);
+}
+
+for(i=1; i<=200; i++){
+    //console.log(i);
+  if(i%2===0){
+  console.log(i);
+    
+   }
+}
+
